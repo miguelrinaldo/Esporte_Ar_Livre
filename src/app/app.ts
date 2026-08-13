0acdf98356800862cbafa21e6978599b67a02e47
Miguel Rinaldo <miguelrinaldo6@gmail.com>
2026-08-12 21:16:22 -0300

    arquivos de esporte ar livre

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MenuComponent } from './component/menu-cpmponent/menu-component';
+import { AtletaComponent } from "./component/atleta-component/atleta-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, AtletaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
