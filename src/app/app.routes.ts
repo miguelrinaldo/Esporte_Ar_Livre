6ddbb2660c3ec621ca8d833217f448d4f69c9a07
Miguel Rinaldo <miguelrinaldo6@gmail.com>
2026-08-12 21:41:08 -0300

    arquivos de esporte ar livre

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './component/menu-cpmponent/menu-component';
+import path from 'path';
+import { HomeComponent } from './component/home-component/home-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+export const routes: Routes = [
+   {
+    path: '',
+    redirectTo:"/home",
+    pathMatch: 'full'
 
-export const routes: Routes = [];
+   },  
+   
+{
+    path: "home",
+    component: HomeComponent
+},
+
+{
+    path:"cadastroatleta",
+    component: AtletaComponent
+}
+];
