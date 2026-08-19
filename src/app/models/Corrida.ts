3f5a48e74b43ec9688bcb91f61fec02315237a3f
Miguel Rinaldo <miguelrinaldo6@gmail.com>
2026-08-19 18:41:39 -0300

    criei um novo service

---
+++ src/app/models/Corrida.ts
@@ -0,0 +1,8 @@
+export class Corrida {
+    id: number = 0
+    descricao_corrida: string = ''
+    data_corrida: string = ''
+    distancia5km: boolean = false
+    distancia10km: boolean = false
+    distancia25km: boolean = false
+}
